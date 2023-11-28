38e244445ac10092c3a947c3b633194e434ea71b
danilgordienko <danilgordienko212@gmail.com>
2023-11-28 13:00:15 +0300

    add navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatelessWidget {
+  const Navigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: SingleChildScrollView(
+            child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left:15, top: 50), 
+          child: Column(
+            children: [
+               ListTile(
+                leading: const Icon(Icons.data_saver_off),
+                title: const Text("статистика"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/statistic');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.timer), 
+                title: const Text("таймер"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/timer');
+                },
+                )
+            ],
+          ),
+        ),
+      ],
+    )));
+  }
+}
