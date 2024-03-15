a06d6f0c93ca192309d1c03681186657ae35c748
nktfmnko <nktfmnko@gmail.com>
2024-03-15 21:30:33 +0300

    Добавлен экран входа

---
+++ lib/login.dart
@@ -0,0 +1,146 @@
+import 'package:cgk/select_questions.dart';
+import 'package:cgk/signup.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+bool rightLogin(String mail, String password) {
+  for (var value in users) {
+    if (value.mail == mail) if (value.password == password) return true;
+  }
+  return false;
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool seePassword = true;
+  final mailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        controller: mailController,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_outline_outlined),
+                          labelText: 'E-Mail',
+                          hintText: 'E-Mail',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        obscureText: seePassword,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.password_outlined),
+                          labelText: 'Пароль',
+                          hintText: 'Пароль',
+                          border: OutlineInputBorder(),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              seePassword
+                                  ? seePassword = false
+                                  : seePassword = true;
+                              setState(
+                                () {},
+                              );
+                            },
+                            icon: Icon(Icons.remove_red_eye_sharp),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 1,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text('Забыли пароль?'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            rightLogin(mailController.text,
+                                    passwordController.text)
+                                ? Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SelectQuestion(),
+                                    ),
+                                    (route) => false)
+                                : showDialog(
+                                    context: context,
+                                    builder: (BuildContext) {
+                                      return AlertDialog(
+                                        title:
+                                            Text('Неверный логин или пароль'),
+                                      );
+                                    },
+                                  );
+                          },
+                          child: Text('Войти'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpScreen(),
+                      ),
+                    );
+                  },
+                  child: Text.rich(
+                    TextSpan(
+                      text: 'Нет аккаунта? ',
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: 'Зарегистрироваться',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
