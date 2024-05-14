03ebbd85d1bf4dc5fcd5d3a9e48d47ae32f8ec66
nktfmnko <nktfmnko@gmail.com>
2024-05-14 20:48:01 +0300

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,16 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1Z2h1ZXVjdXppbnRodG9ya3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0ODQ2NDcsImV4cCI6MjAxNjA2MDY0N30.1IXKCtjMpNB0DJ2_ZBixn59MSr7mlxVeebTSYHjzlFY',
   );
-  // SystemChrome.setSystemUIOverlayStyle(
-  //   const SystemUiOverlayStyle(
-  //     systemStatusBarContrastEnforced: true,
-  //     systemNavigationBarColor: Colors.transparent,
-  //     systemNavigationBarDividerColor: Colors.transparent,
-  //     systemNavigationBarIconBrightness: Brightness.light,
-  //     statusBarIconBrightness: Brightness.light,
-  //   ),
-  // );
-  // SystemChrome.setEnabledSystemUIMode(
-  //   SystemUiMode.edgeToEdge,
-  //   overlays: [SystemUiOverlay.top],
-  // );
+   SystemChrome.setSystemUIOverlayStyle(
+     const SystemUiOverlayStyle(
+       systemStatusBarContrastEnforced: true,
+       systemNavigationBarColor: Color(0xff4397de),
+       systemNavigationBarDividerColor: Colors.transparent,
+       systemNavigationBarIconBrightness: Brightness.light,
+       statusBarIconBrightness: Brightness.light,
+     ),
+  );
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   isRemembered();
   runApp(const MyApp());
@@ -49,8 +46,10 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> initDeepLinks() async {
     _appLinks = AppLinks();
+
+    // Handle links
     _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
-      print('onAppLink: $uri');
+      print('onAppLink: ${uri}');
     });
   }
 
@@ -78,3 +77,13 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+/*
+* return MaterialApp(
+      home: isLogin ? const menu() : const LoginScreen(),
+      theme: ThemeData(
+        textSelectionTheme: TextSelectionThemeData(
+          selectionHandleColor: Colors.white,
+        ),
+      ),
+    );
+* */
